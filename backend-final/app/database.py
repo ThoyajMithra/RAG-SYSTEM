cc26e04b66add4793fab14907540d02c046cfb18
ThoyajMithra <thoyajmithra17@gmail.com>
2026-07-25 22:38:38 +0530

    fixed database os url

--- backend-final/app/database.py
+++ backend-final/app/database.py
@@ -1,9 +1,12 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-from app.config import settings
+import os
 
-engine = create_engine(settings.DATABASE_URL)
+
+DATABASE_URL = os.environ["DATABASE_URL"]
+engine = create_engine(DATABASE_URL)
+
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
